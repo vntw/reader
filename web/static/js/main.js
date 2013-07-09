1ac9d626b02d4fef0555eb3a9e5e4ce2707516a3
venyii <schefflor@gmail.com>
2013-07-09 13:29:46 +0200

    move read/saved to js

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,82 +1,61 @@
-$(function () {
+var resizeTimer;
+$(window).resize(function () {
+	clearTimeout(resizeTimer);
+	resizeTimer = setTimeout(Reader.Layout.Fix, 100);
+});
+Reader.Layout.Fix();
 
-    var resizeTimer;
-    $(window).resize(function () {
-        clearTimeout(resizeTimer);
-        resizeTimer = setTimeout(Reader.Layout.Fix, 100);
-    });
-    Reader.Layout.Fix();
+$(document).on('click', 'a.tag-container', function (e) {
+	e.preventDefault();
 
-    $(document).on('click', 'a.tag-container', function (e) {
-        e.preventDefault();
+	var $this = $(this),
+		icon = $('> i', $this);
 
-        var $this = $(this),
-            icon = $('> i', $this);
+	$this.next('ul').slideToggle(200);
 
-        $this.next('ul').slideToggle(200);
+	if (icon.hasClass('icon-folder-close')) {
+		icon.addClass('icon-folder-open').removeClass('icon-folder-close');
+	} else {
+		icon.addClass('icon-folder-close').removeClass('icon-folder-open');
+	}
+});
 
-        if (icon.hasClass('icon-folder-close')) {
-            icon.addClass('icon-folder-open').removeClass('icon-folder-close');
-        } else {
-            icon.addClass('icon-folder-close').removeClass('icon-folder-open');
-        }
-    });
+$(document).on('click', 'div.item a.item-favourite i', function (e) {
+	e.preventDefault();
 
-    $(document).on('click', 'div.item a.item-favourite i', function (e) {
-        e.preventDefault();
+	var $this = $(this),
+		$item = $this.parents('div.item'),
+		add = $this.hasClass('icon-star-empty');
 
-        var $this = $(this),
-            $item = $this.parents('div.item'),
-            add = $this.hasClass('icon-star-empty');
+	Reader.Items.Mark(Reader.Items.Funcs.FAVOURITES, (add ? 'add' : 'del'), $item.data('item-id'), $item, $this);
+});
 
-        Reader.Items.Mark(Reader.Items.Funcs.FAVOURITES, (add ? 'add' : 'del'), $item.data('item-id'));
+$(document).on('click', 'div.item a.item-markread i', function (e) {
+	e.preventDefault();
 
-        if (add) {
-            $this.addClass('icon-star').removeClass('icon-star-empty');
-        } else {
-            $this.addClass('icon-star-empty').removeClass('icon-star');
-        }
-    });
+	var $this = $(this),
+		$item = $this.parents('div.item'),
+		add = $this.hasClass('icon-eye-open');
 
-    $(document).on('click', 'div.item a.item-markread i', function (e) {
-        e.preventDefault();
+	Reader.Items.Mark(Reader.Items.Funcs.READ, (add ? 'add' : 'del'), $item.data('item-id'), $item, $this);
+});
 
-        var $this = $(this),
-            $item = $this.parents('div.item'),
-            add = $this.hasClass('icon-eye-open');
+$(document).on('click', 'div.item a.item-save i', function (e) {
+	e.preventDefault();
 
-        Reader.Items.Mark(Reader.Items.Funcs.READ, (add ? 'add' : 'del'), $item.data('item-id'));
+	var $this = $(this),
+		$item = $this.parents('div.item'),
+		add = $this.hasClass('icon-time');
 
-        if (add) {
-            $this.addClass('icon-eye-close').removeClass('icon-eye-open');
-            $item.find('span.item-title').removeClass('unread');
-        } else {
-            $this.addClass('icon-eye-open').removeClass('icon-eye-close');
-            $item.find('span.item-title').addClass('unread');
-        }
-    });
+	Reader.Items.Mark(Reader.Items.Funcs.SAVED, (add ? 'add' : 'del'), $item.data('item-id'), $item, $this);
+});
 
-    $(document).on('click', 'div.item a.item-save i', function (e) {
-        e.preventDefault();
+$(document).on('click', 'span.item-title, span.item-preview, div.item-sub-name, div.item-date', function (e) {
+	var $this = $(this),
+		$item = $this.parents('div.item');
 
-        var $this = $(this),
-            $item = $this.parents('div.item'),
-            add = $this.hasClass('icon-time');
+	$('div.item-collapsed', $item).toggleClass('collapsed');
+	$('div.item-container', $item).toggleClass('hide');
 
-        Reader.Items.Mark(Reader.Items.Funcs.SAVED, (add ? 'add' : 'del'), $item.data('item-id'));
-
-        if (add) {
-            $this.addClass('icon-ok-sign').removeClass('icon-time');
-        } else {
-            $this.addClass('icon-time').removeClass('icon-ok-sign');
-        }
-    });
-
-    $(document).on('click', 'span.item-title, span.item-preview, div.item-sub-name, div.item-date', function (e) {
-        var $this = $(this),
-            $item = $this.parents('div.item');
-
-        $('div.item-collapsed', $item).toggleClass('collapsed');
-        $('div.item-container', $item).toggleClass('hide');
-    });
+	Reader.Items.Mark(Reader.Items.Funcs.READ, 'add', $item.data('item-id'), $item, $('a.item-markread i', $item));
 });
